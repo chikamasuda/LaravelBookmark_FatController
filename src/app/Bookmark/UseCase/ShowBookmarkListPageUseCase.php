eb2a3c0fd21386bbc95a985ae9ef2efa37c08364
chikamasuda <c.m.062820@gmail.com>
2022-09-28 23:42:38 +0900

    CreateBookmarkUseCaseクラスの作成

--- src/app/Bookmark/UseCase/ShowBookmarkListPageUseCase.php
+++ src/app/Bookmark/UseCase/ShowBookmarkListPageUseCase.php
@@ -1,2 +1,13 @@
 <?php
 
+namespace App\Bookmark\UseCase;
+
+use App\Models\Bookmark;
+use Dusterio\LinkPreview\Client;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+
+final class CreateBookmarkUseCase
+{
+}
