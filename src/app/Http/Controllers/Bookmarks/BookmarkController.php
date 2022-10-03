8fd6b105941563ea6bfff8d08c5e7f25559bdf71
chikamasuda <c.m.062820@gmail.com>
2022-10-03 14:30:02 +0900

    新規 FormRequestのCreateBookmarkRequestを作成

--- src/app/Http/Controllers/Bookmarks/BookmarkController.php
+++ src/app/Http/Controllers/Bookmarks/BookmarkController.php
@@ -3,7 +3,6 @@
 
 namespace App\Http\Controllers\Bookmarks;
 
-
 use App\Http\Controllers\Controller;
 use App\Models\Bookmark;
 use App\Models\BookmarkCategory;
@@ -22,6 +21,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
+use App\Http\Requests\CreateBookmarkRequest;
 
 class BookmarkController extends Controller
 {
@@ -123,18 +123,12 @@ class BookmarkController extends Controller
      * @param Request $request
      * @return Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function create(Request $request)
+    public function create(CreateBookmarkRequest $request)
     {
         if (Auth::guest()) {
             // @note ここの処理はユーザープロフィールでも使われている
             return redirect('/login');
         }
-
-        Validator::make($request->all(), [
-            'url' => 'required|string|url',
-            'comment' => 'required|string|min:10|max:1000',
-            'category' => 'required|integer|exists:bookmark_categories,id',
-        ])->validate();
 
         // 下記のサービスでも同様のことが実現できる
         // @see https://www.linkpreview.net/
