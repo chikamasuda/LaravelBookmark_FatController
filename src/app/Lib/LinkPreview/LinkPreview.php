d9f8ea00afab44964a12df5e15f738c24556ff6f
chikamasuda <c.m.062820@gmail.com>
2022-10-09 15:40:02 +0900

    LinkPreviewクラスに対応したインターフェースを定義

--- src/app/Lib/LinkPreview/LinkPreview.php
+++ src/app/Lib/LinkPreview/LinkPreview.php
@@ -4,11 +4,11 @@ namespace App\Lib\LinkPreview;
 
 use Dusterio\LinkPreview\Client;
 
-final class LinkPreview
+final class LinkPreview implements LinkPreviewInterface
 {
   public function get(string $url): GetLinkPreviewResponse
   {
-    $previewClient = newClient($url);
+    $previewClient = new Client($url);
     $response = $previewClient->getPreview('general')->toArray();
     return new GetLinkPreviewResponse($response['title'], $response['description'], $response['cover']);
   }
